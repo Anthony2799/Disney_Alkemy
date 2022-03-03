b0bc099ff262f1829f4f81419b947fd5245c08fe
Anthony27111999 <sosa27111999@gmail.com>
2022-03-03 01:03:37 -0300

    Agrego librerias, rutas y arreglos al gitignore

---
+++ Disney/router/generalRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const PersonajeRouter = require('./PersonajeRouter');
+
+function darRutas(app){
+    const router = express.Rputer();
+    app.use('/api/v1',router);
+    router.use('/personaje', PersonajeRouter);
+}
